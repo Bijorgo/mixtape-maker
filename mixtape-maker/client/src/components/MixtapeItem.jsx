221e3d150a0e7217454e6684d7538f4d6c3a2043
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2025-02-12 10:46:14 -0700

    create mixtapeIte component

--- mixtape-maker/client/src/components/MixtapeItem.jsx
+++ mixtape-maker/client/src/components/MixtapeItem.jsx
@@ -1,16 +1,26 @@
-// Display song name, artist, album, and listen status. Include options to mark as "listened" or "unlistened."
-import { useState, useEffect } from "react"
+// Display mixtape preview in Home
+import { Link } from "react-router-dom"
 
-export default function MixtapeItem(){
-    // States
-    const [ mixtapes, setMixtapes ] = useState([])
-    // Initital fetch GET request
-    useEffect(() => {
-        fetch("http://localhost:5000/mixtapes")
-        .then( r => r.json() )
-        .then( mixtapeData => setMixtapes(mixtapeData))
-        .catch( error => console.log(error))
-    },[])
-    // Fetch GET request
-
+export default function MixtapeItem({ mixtapes }){
+    return(
+        <div>
+            <h2>My Mixtapes: </h2>
+            <div>
+                {mixtapes.map( mixtape => (
+                    <div
+                        key={mixtape.id}
+                        //className=""
+                    >
+                        <p>Something to display the mixtape goes here</p>
+                        <Link
+                        to={`/???/$mixtape.id`}
+                        //className=""
+                        >
+                            View Mixtape Contents
+                        </Link>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
--- mixtape-maker/client/src/components/MixtapeView.jsx
+++ mixtape-maker/client/src/components/MixtapeView.jsx
@@ -1,4 +1,5 @@
-// Display a specific mixtape (Home page)
+// Display song name, artist, album, and listen status. Include options to mark as "listened" or "unlistened."
+// Links from Home
 export default function MixtapeView(){
     
 }
