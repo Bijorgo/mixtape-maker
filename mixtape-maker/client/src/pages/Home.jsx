d08393132ee7712aa84e6299879741c183b73c23
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2025-02-12 09:24:37 -0700

    Home page set up

---
+++ mixtape-maker/client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react"
+import MixtapeForm from "../components/MixtapeForm"
+
+function Home(){
+    //Landing page where users can view available mixtapes
+    return(
+        <div>
+
+        </div>
+    )
+}
