f7fa4ce0e430874348584bc0b8a4cbfc0a508bc0
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2025-02-12 09:32:07 -0700

    add export default

--- mixtape-maker/client/src/pages/Home.jsx
+++ mixtape-maker/client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 import MixtapeForm from "../components/MixtapeForm"
 
-function Home(){
+export default function Home(){
     //Landing page where users can view available mixtapes
     return(
         <div>
