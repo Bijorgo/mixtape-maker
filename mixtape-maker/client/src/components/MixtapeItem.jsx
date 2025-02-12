55a186426f42ac399490fd7035546ec4de144216
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2025-02-12 09:26:38 -0700

    export default on components

--- mixtape-maker/client/src/components/MixtapeItem.jsx
+++ mixtape-maker/client/src/components/MixtapeItem.jsx
@@ -1,5 +1,5 @@
 // Display song name, artist, album, and listen status. Include options to mark as "listened" or "unlistened."
-function MixtapeItem(){
+export default function MixtapeItem(){
     // States
     // Fetching with useEffect (async)
 }
