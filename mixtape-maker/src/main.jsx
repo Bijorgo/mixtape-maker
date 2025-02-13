52423fa7d88c038608d8c084836f9d6ba6ef2c93
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2025-02-12 19:40:56 -0700

    add navbar

--- mixtape-maker/src/main.jsx
+++ mixtape-maker/src/main.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './App.css'
-import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>,
-)
+// Importing Components
+import DeleteSong from "./components/DeleteSong.jsx";
+import MixtapeContents from "./components/MixtapeContents.jsx";
+import MixtapeDisplay from "./components/MixtapeDisplay.jsx";
+import MixtapeForm from "./components/MixtapeForm.jsx";
+import SongForm from "./components/SongForm.jsx";
+import SongList from "./components/SongList.jsx";
+import SongSearch from "./components/SongSearch.jsx";
+import StatusToggle from "./components/StatusToggle.jsx";
+import UserLogin from "./components/UserLogin.jsx";
+import UserRegistration from "./components/UserRegistration.jsx";
+import App from "./App.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
+
+// Define Routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <>
+            <UserRegistration />
+            <UserLogin />
+            <MixtapeDisplay />
+            <MixtapeForm />
+          </>
+        ),
+      },
+      {
+        path: "songs",
+        element: (
+          <>
+            <SongForm />
+            <DeleteSong />
+            <SongSearch />
+            <SongList />
+            <StatusToggle />
+          </>
+        ),
+      },
+      {
+        path: "mixtapecontent",
+        element: (
+          <>
+            <MixtapeContents />
+            <StatusToggle />
+          </>
+        ),
+      },
+    ],
+  },
+]);
+
+// Render the Router
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
