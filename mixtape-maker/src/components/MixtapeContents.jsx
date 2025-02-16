8317bf1852c3d1c96015f61ac1a628c51dd3673a
Daniel Oshima <doshima2013@gmail.com>
2025-02-16 14:13:13 -0500

    add SongForm import line

--- mixtape-maker/src/components/MixtapeContents.jsx
+++ mixtape-maker/src/components/MixtapeContents.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import StatusToggle from "./StatusToggle"; // Import the StatusToggle component
+import SongForm from "./SongForm"
 
 export default function MixtapeContents() {
     const navigate = useNavigate();
