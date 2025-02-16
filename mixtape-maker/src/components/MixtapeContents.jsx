f48b462acc5380c8ab3998744c3451923cbe32db
Daniel Oshima <doshima2013@gmail.com>
2025-02-16 14:06:46 -0500

    add SongForm component to MixtapeContents component

--- mixtape-maker/src/components/MixtapeContents.jsx
+++ mixtape-maker/src/components/MixtapeContents.jsx
@@ -142,6 +142,8 @@ export default function MixtapeContents() {
                         <p className="text-gray-500">No songs found in this mixtape.</p>
                     )}
                 </ul>
+                <h3 className="text-xl font-semibold text-gray-800">Add A New Song To This Mixtape:</h3>
+                <SongForm mixtapeId={mixtape.id} />
             </div>
         </div>
     );
